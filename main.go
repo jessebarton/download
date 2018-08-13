dbe3bc2eb4f94b6074cc474b80851554f67a9599
Jesse Barton <jesse.barton@life.church>
2018-08-13 13:20:46 -0500

    added fileInfo()

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"net/url"
 	"os"
@@ -12,10 +13,27 @@ import (
 func main() {
 	arg := os.Args[1]
 	name := getURLName(arg)
+
 	err := downloadFile(name, arg)
 	if err != nil {
 		panic(err)
 	}
+
+	f := fileInfo(name)
+	fmt.Printf("\nName	Size\n%s	%vk\n ", f.Name(), f.Size())
+
+}
+
+func fileInfo(name string) os.FileInfo {
+	file, err := os.Open(name)
+	if err != nil {
+		log.Fatalf("Failed to read file: %v", err)
+	}
+	stat, err := file.Stat()
+	if err != nil {
+		log.Fatalf("Failed to get stats: %v", err)
+	}
+	return stat
 }
 
 func getURLName(arg string) string {
@@ -40,7 +58,7 @@ func downloadFile(filepath string, url string) error {
 	}
 	defer out.Close()
 
-	fmt.Printf("Downloading File: %s", filepath)
+	fmt.Printf("Downloading File: %s\n", filepath)
 	resp, err := http.Get(url)
 	if err != nil {
 		return err
